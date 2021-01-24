8b160949fa5ef97bedd9c24d5160168fadc9c89f
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2021-01-24 17:00:23 -0600

    Callbacks in node.js

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 'use strict'
 
-const math = require('./math.js');
+const os = require('os');
+const fs = require('fs');
 
-console.log(math);
+// Using os module
+console.log(os.platform());
+console.log(os.release());
+console.log(`Total memory: ${os.totalmem()} bytes`);
+console.log(`Free memory: ${os.freemem()} bytes`);
 
-console.log(math.add(1,2));
-console.log(math.substract(1,2));
-console.log(math.multiply(1,2));
-console.log(math.divide(1,2));
-console.log(math.divide(1,0));
+// Using fs module
+// Callback -> Function executed when the previous process has finished
+fs.writeFile('./text.txt', 'first line', function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('File successfully created');
+  }
+});
+
+console.log('Last line of code');
+
+fs.readFile('./text.txt', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
+
+console.log(`Hi I'm waiting for the Async functions response`);
+// const user = query('SELECT * FROM Users ');
+
+// query('SELECT * FROM Users', function (err, users) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   if (users) {
+
+//   }
+// })
+
