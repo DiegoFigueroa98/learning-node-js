f56fbb8bfd103dc14540472d1335e23568e6f0df
Diego Aaron Figueroa Campos <16460315@colima.tecnm.mx>
2021-01-25 16:42:34 -0600

    First steps with Express and Node.js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 'use strict'
 
-const http = require('http');
+// const http = require('http');
 
-const handleServer = function (req, res) {
-  res.writeHead(200, { 'Content-type': 'text/html' });
-  res.write('This is a flat text');
+// const colors = require('colors');
+
+// const handleServer = function (req, res) {
+//   res.writeHead(200, { 'Content-type': 'text/html' });
+//   res.write('This is a flat text');
+//   res.end();
+// }
+
+// const server = http.createServer(handleServer);
+
+// server.listen(3000, function () {
+//   console.log('Server on port 3000'.yellow);
+// });
+
+const express = require('express');
+
+const colors = require('colors');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('<h1>Hello world with Express + Node.js</h1>');   
   res.end();
-}
-
-const server = http.createServer(handleServer);
-
-server.listen(3000, function () {
-  console.log('Server on port 3000');
 });
+
+server.listen(3000, () => {
+  console.log('Server on port 3000'.red);
+})
